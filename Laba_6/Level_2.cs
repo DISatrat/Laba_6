ad23f509a179b41cfa0ad8123a00d0103c0f4401
DISatrat <dimaneopal@mail.ru>
2023-02-10 16:49:58 +0300

    gdgd

---
+++ Laba_6/Level_2.cs
@@ -0,0 +1,57 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Laba_6
+//{
+//    class Level_2
+//    {
+//        static void Main(string[] args)
+//        {
+//            N1();
+//        }
+//        static void N1()
+//        {
+//            Student[] st = new Student[10];
+//            st[0] = new Student("Ivan", 3, 5, 2, 4);
+//            st[1] = new Student("Max", 4, 5, 4, 4);
+//            st[2] = new Student("Andrey", 5, 5, 5, 5);
+//            st[3] = new Student("Dima", 3, 5, 4, 5);
+//            st[4] = new Student("Ivan_2", 4, 2, 4, 4);
+//            st[5] = new Student("Kirill", 3, 3, 2, 4);
+//            st[6] = new Student("Kirill_2", 2, 2, 2, 2);
+//            st[7] = new Student("Mash", 4, 5, 3, 2);
+//            st[8] = new Student("Vika", 5, 5, 4, 3);
+//            st[9] = new Student("Vitia", 3, 3, 3, 4);
+            
+//            for (int i = 0; i < st.Length; i++)
+//            {
+//                double sred = (st[i].Math + st[i].Proga + st[i].Phisics + st[i].History)/4;
+                
+//                if (sred >= 4)
+//                {
+//                    Console.WriteLine(st[i].Name + " средний балл " + sred);  
+//                }
+//            }
+
+//        }
+//        struct Student
+//        {
+//            public string Name;
+//            public double Math;
+//            public double Proga;
+//            public double Phisics;
+//            public double History;
+
+//            public Student(string Name, double Math, double Proga, double Phisics, double History)
+//            {
+//                this.Name = Name;
+//                this.Math = Math;
+//                this.Proga = Proga;
+//                this.Phisics = Phisics;
+//                this.History = History;
+//            }
+
+//        }
+//    }
+//}
---
+++ Laba_6/Level_3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace Laba_6
+{
+    class Level_3
+    {
+        static void Main(string[] args)
+        {
+            N1();
+        }
+        static void N1()
+        {
+            Group[] s1 = new Group[1];
+            Group[] s2 = new Group[5];
+            Group[] s3 = new Group[5];
+
+            string[] Name1 = new string[] { "Sasha", "Andrey", "Max", "Ivan", "Nikita", "Misha" };
+            int[] Mark = new int[] { 2, 3, 4, 5 };
+           
+            for (int i = 0; i < s2.Length; i++)
+            {
+                int[]r= RandomMarks(5, 2, 6);
+                s2[i] = new Group(Name1[1], r);
+                Console.WriteLine(s2[i].Name +" ");
+            }
+            for (int i = 0; i < s2.Length; i++)
+            {
+                Console.Write(s2[i].Marks[i]+" ");
+
+            }
+        }
+            
+        struct Group
+        {
+           public string Name;
+          public  int[] Marks;
+ 
+            public Group(string Name, int[] Marks)
+            {
+                this.Name = Name;
+                this.Marks = Marks;
+            }
+        }
+        static int[] RandomMarks(int h, int k, int l)
+        {
+            Random random = new Random();
+            int[] Marks = new int[h];
+            for (int i = 0; i < Marks.Length; i++)
+            {
+                Marks[i] = random.Next(k, l);
+            }
+            return Marks;
+        }
+    }
+}
